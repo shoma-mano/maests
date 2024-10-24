fa97a9c30f999096ba21f18d3b72558fd3b8e3a2
shoma-mano <ms2geki@gmail.com>
2024-10-24 20:20:52 +0900

    remove unnesessary code

--- example/sample-flow.maestro.ts
+++ example/sample-flow.maestro.ts
@@ -1,26 +1,20 @@
-import createJITI from 'jiti'
+import { M } from "maests";
 
-import {M} from 'maests'
+M.initFlow({ appId: "com.my.app", NAME: "Maestro" });
 
-const str:string='test'
+M.launchApp({ appId: "com.my.app" });
 
-M.initFlow({ appId: "com.my.app", NAME: "Maestro" })
+M.tapOn("buttonId");
+M.tapOn("otherButtonId");
 
-M.launchApp({ appId: "com.my.app" })
+M.inputText("Hello World", "textFieldId");
 
-M.tapOn("buttonId")
-M.tapOn("otherButtonId")
-
-console.log('test')
-
-M.inputText("Hello World", "textFieldId")
-
-M.inputText("{NAME}", "otherTextFieldId")
-M.inputText("more text for focused input")
+M.inputText("{NAME}", "otherTextFieldId");
+M.inputText("more text for focused input");
 
 // This would only work once you've set up deep linking in your app
 // and you've set the appropriate deepLinkingBase in maestro.config.ts
-M.navigate("/profile")
+M.navigate("/profile");
 
 // If you apply routes as testIDs to your ScreenContainers, you can do some pretty cool stuff 😎
-M.assertVisible("/profile")
+M.assertVisible("/profile");
