71712ade43e6133630d53e966615cbe38d152caa
shoma-mano <ms2geki@gmail.com>
2024-10-27 22:42:22 +0900

    chore: update playground

--- playground/e2e/script.ts
+++ playground/e2e/script.ts
@@ -1,6 +1,5 @@
 import type { APIResult } from "./type";
 
-const result = http.get(
-  "https://jsonplaceholder.typicode.com/todos/1"
-) as APIResult;
-output.userId = result.userId.toString();
+const body = http.get("https://jsonplaceholder.typicode.com/todos/1").body;
+const result = json<APIResult>(body);
+console.log(result.userId);
--- playground/maests/e2e/runScript.temp.ts
+++
@@ -1,8 +0,0 @@
-import { M, writeYaml } from 'maests'
-import { join } from "path";
-
-M.initFlow({ appId: "com.my.app" });
-M.runScript({ path: join(__dirname, "script.ts") });
-M.stopApp();
-
-writeYaml("/Users/mano/my-oss/maests/playground/maests/e2e/runScript.yaml")
