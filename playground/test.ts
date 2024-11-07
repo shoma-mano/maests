e1233a7cce48979a3d00de3d2b5402644364cd55
shoma-mano <ms2geki@gmail.com>
2024-11-07 12:33:33 +0900

    test: android-emulator-runnner

---
+++ playground/test.ts
@@ -0,0 +1,11 @@
+import { execSync, spawn } from "child_process";
+
+const test = async () => {
+  const playgroundDir = __dirname;
+  const expoProc = spawn(`cd ${playgroundDir} && npx expo run:android`, {
+    stdio: "inherit",
+  });
+  execSync(`npx wait-on tcp:localhost:8081`);
+  expoProc.kill();
+};
+test();
