acb9776dd0c88e8e5a9d214d51820019d24ae2b3
shoma-mano <ms2geki@gmail.com>
2024-11-08 02:05:16 +0900

    test: android-emulator-runnner

--- playground/test.ts
+++ playground/test.ts
@@ -1,13 +1,15 @@
-import { execSync, spawn } from "child_process";
+import { execSync, spawn, spawnSync } from "child_process";
 
 const test = async () => {
   console.log("Starting test");
-  execSync("npx expo run:android", {
+  const proc = spawn("npx", ["expo", "run:android"], {
     stdio: "inherit",
   });
+  execSync(`npx wait-on tcp:localhost:8081`);
   execSync("npx maests e2e/sampleFlow.ts", {
     cwd: __dirname,
     stdio: "inherit",
   });
+  proc.kill("SIGKILL");
 };
 test();
